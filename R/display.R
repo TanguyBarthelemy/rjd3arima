88e70b4d641c26f2eade2cb5b8c128441658e532
AQLT <alain.quartier@yahoo.fr>
2022-04-01 16:49:24 +0200

    print.JD3_SPAN correction

--- R/display.R
+++ R/display.R
@@ -170,8 +170,8 @@ print.JD3_SPAN<-function(x, ...){
   else if (type=="FROM") {x<-paste("From",d0, sep=" ")}
   else if (type=="TO") {x<-paste("Until",d1, sep=" ")}
   else if (type=="BETWEEN") {x<-paste(d0,d1,sep=" - ")}
-  else if (type=="FIRST") {x<-paste("All but first",n0,"periods", sep=" ")}
-  else if (type=="LAST") {x<-paste("All but last",n1,"periods", sep=" ")}
+  else if (type=="FIRST") {x<-paste("First",n0,"periods", sep=" ")}
+  else if (type=="LAST") {x<-paste("Last",n1,"periods", sep=" ")}
   else if (type=="EXCLUDING") {x<-paste("All but first",n0,"periods and last",n1,"periods", sep=" ")}
   else {x<- "Undefined"}
 
